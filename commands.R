920b78ae4d8ae93c3e1346166358dc07a42a0abe
shadrinams <shadrinams@gmail.com>
2017-04-06 15:09:53 -0400

    Update commands.R

--- commands.R
+++ commands.R
@@ -6,6 +6,10 @@ qplot(data=data,x=X29DI, y=X30DI, alpha=I(0.01))
 
 data <- read.csv("runs\\wt_run1\\RAMACHANDRAN.dat", colClasses = c(rep("numeric")), sep="", head=F)
 mat=as.matrix(data)
+
+resmat[lower.tri(resmat)] <- NA
+heatmap(resmat, symm=F, Rowv = NA, Colv = NA, scale='none', revC=F, col = colorRampPalette(c("white","black"))(256))
+
 names(mat) <- seq(-180, 180, 6)
 row.names(mat) <- seq(-180, 180, 6)
 heatmap(mat, symm=T, Rowv = NA, Colv = NA, scale='none', revC=T, col = colorRampPalette(c("red","white","blue"))(256))
