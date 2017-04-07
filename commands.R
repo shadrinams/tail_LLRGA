214c40daf921c5ea44588fb238408587af64234b
shadrinams <shadrinams@gmail.com>
2017-04-07 14:26:44 -0400

    Update commands.R

--- commands.R
+++ commands.R
@@ -13,3 +13,4 @@ heatmap(resmat, symm=F, Rowv = NA, Colv = NA, scale='none', revC=F, col = colorR
 names(mat) <- seq(-180, 180, 6)
 row.names(mat) <- seq(-180, 180, 6)
 heatmap(mat, symm=T, Rowv = NA, Colv = NA, scale='none', revC=T, col = colorRampPalette(c("red","white","blue"))(256))
+heatmap(mat, symm=F, Rowv = NA, Colv = NA, scale='none', revC=F, col = colorRampPalette(c("red","white","blue"))(299), breaks=c(seq(-1,-0.33,length=100),seq(-0.33,0.33,length=100),seq(0.33,1,length=100)))  #assign colors
